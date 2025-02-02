5a6673326a149df07af1a22aacf430f3b2ca1e44
HarshK200 <harshkapadia04@gmail.com>
2025-02-03 04:31:02 +0530

    some minute adjustments to register ui

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -5,9 +5,9 @@ export default function Register() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white sm:bg-slate-100">
       <div className="sm:shadow-xl sm:w-[450px] rounded-xl p-8 bg-white">
-        <div className="flex items-center pt-2 pb-8 text-2xl">
-          <JaeCart width={0.46} />
-          <h1 className="mx-5">Get Started</h1>
+        <div className="flex items-center py-2 pb-8">
+          <JaeCart width={0.50} />
+          <h1 className="mx-4 font-bold text-primary text-3xl">JAE</h1>
         </div>
         <RegisterForm />
       </div>
