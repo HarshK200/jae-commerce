75bc315126165a8828bc0c5dedbbfea617374e4c
HarshK200 <harshkapadia04@gmail.com>
2025-02-25 12:45:27 +0530

    added currency formatting

--- src/app/(main)/seller/page.tsx
+++ src/app/(main)/seller/page.tsx
@@ -1,9 +1,32 @@
 import SellerDashboardCard from "@/components/(Seller)/SellerDashBoardCard";
+import { prisma as db } from "@/db";
+import { formatNumber, formatCurrency } from "@/lib/formatters";
+
+async function getSales(userId: string) {
+  const data = await db.order.aggregate({
+    where: {
+      user_id: userId,
+    },
+    _sum: { price_paid: true },
+    _count: true,
+  });
+
+  return {
+    amount: data._sum.price_paid?.toNumber() || 0,
+    numberOfOrders: data._count,
+  };
+}
 
 export default async function SellerDashboardPage() {
+  const salesData = await getSales("2");
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      <SellerDashboardCard title="Sales" subtitle="Test" body="$ 20" />
+      <SellerDashboardCard
+        title="Sales"
+        subtitle={formatNumber(salesData.numberOfOrders)}
+        body={formatCurrency(salesData.amount)}
+      />
     </div>
   );
 }
