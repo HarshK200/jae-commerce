988b803eec358a88014953b6627b350c926522b5
HarshK200 <harshkapadia04@gmail.com>
2025-03-22 16:15:44 +0530

    fixed ipad size categories bug

--- src/components/CategriesBar.tsx
+++ src/components/CategriesBar.tsx
@@ -6,13 +6,17 @@ export async function CategoryBar() {
   const productCategories = await db.productCategory.findMany();
 
   return (
-    <div className="flex overflow-x-auto w-full md:justify-center">
-      <CategoryBtn categoryName={"All Category"}>
-        <Menu width={20} />
-      </CategoryBtn>
-      {productCategories.map((category) => {
-        return <CategoryBtn key={category.name} categoryName={category.name} />;
-      })}
+    <div className="flex overflow-x-auto w-full">
+      <div className="flex mx-auto">
+        <CategoryBtn categoryName={"All Category"}>
+          <Menu width={20} />
+        </CategoryBtn>
+        {productCategories.map((category) => {
+          return (
+            <CategoryBtn key={category.name} categoryName={category.name} />
+          );
+        })}
+      </div>
     </div>
   );
 }
